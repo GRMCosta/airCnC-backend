3869e70596cd249adc8e0108e846a71444ede859
GRMCosta <rafa_gian2011@hotmail.com>
2020-01-06 23:24:27 -0300

    Upload imagens em AWS

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+//const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+//const s3 = new AWS.S3();
 
 const SpotSchema = new mongoose.Schema({
     thumbnail: String,
+    url_image: String,
     company: String,
     price: Number,
     techs: [String],
@@ -10,5 +17,24 @@ const SpotSchema = new mongoose.Schema({
         ref: 'User'
     }
 });
+
+SpotSchema.pre('save', function(){
+    if(!this.url_image){
+        this.url_image = `${process.env.APP_URL}/files/${this.thumbnail}`;
+    }
+})
+
+/*SpotSchema.pre("remove", function(){
+    if(process.env.STORAGE_TYPE === "s3"){
+        return s3.deleteObject({
+            Bucket: "uploadmyexample",
+            key: this.thumbnail
+        }).promise();
+    }else{
+        return promisify(fs.unlink)(
+            path.resolve(__dirname, '..','..','uploads', this.thumbnail)
+        );
+    }
+})*/
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
-mongoose.connect('mongodb+srv://tableMeal:tableMeal@tablemeal-n6whx.mongodb.net/semana09?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
    useNewUrlParser: true,
    useUnifiedTopology: true, 
 });
@@ -15,6 +18,11 @@ mongoose.connect('mongodb+srv://tableMeal:tableMeal@tablemeal-n6whx.mongodb.net/
 
 app.use(cors());
 app.use(express.json()); //Muito importante
+app.use(
+   "/files",
+   express.static(path.resolve(__dirname,'..','uploads'))
+)
+
 app.use(routes);
 
 
